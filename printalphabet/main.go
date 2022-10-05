9abcf55cb6751eeb9f35a9419596790701686bab
Yapi Théau <theau.yapi@ynov.comgit config --global user.name ytheau>
2022-10-05 21:45:19 +0200

    printalphabet

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -1,16 +1,11 @@
 package main
 
-import (
-	"piscine/go/pkg/mod/github.com/01-edu/z01@v0.1.0"
-
-	"github.com/01-edu/z01"
-)
+import "github.com/01-edu/z01"
 
 func main() {
-
-	for i := 97; i < 122; i++ {
-		z01.PrintRune
-
+	for i := 97; i <= 122; i++ {
+		c := rune(i)
+		z01.PrintRune(c)
 	}
-
+	z01.PrintRune('\n')
 }
