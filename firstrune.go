53e58990722b30cd41f908a433f052f6e26b6e01
Yapi Théau <theau.yapi@ynov.comgit config --global user.name ytheau>
2022-10-07 14:53:27 +0200

    firstrune

--- firstrune.go
+++ firstrune.go
@@ -1,14 +1,5 @@
 package piscine
 
-/*
-func FirstRune() {
-	z01.PrintRune(Firstrune("Hello!"))
-	z01.PrintRune(Firstrune("Salut!"))
-	z01.PrintRune(Firstrune("Ola!"))
-	z01.PrintRune('\n')
-}
-*/
-
 func Firstrune(s string) rune {
 	r := rune(s[0])
 	return r
