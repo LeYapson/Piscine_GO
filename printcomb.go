97bbad2b274025d521da887a1a4a00307b2e3e64
Yapi Théau <theau.yapi@ynov.comgit config --global user.name ytheau>
2022-10-07 09:41:30 +0200

    printcomb

--- printcomb.go
+++ printcomb.go
@@ -10,7 +10,7 @@ func PrintComb() {
 			for k := 48; k <= 57; k++ {
 				c := rune(k)
 
-				if i < j && j < k  {
+				if i < j && j < k {
 					z01.PrintRune(a)
 					z01.PrintRune(b)
 					z01.PrintRune(c)
