94c204c8ee7ca50952a7f2def25ceaf23a3a6a1c
Yapi Théau <theau.yapi@ynov.comgit config --global user.name ytheau>
2022-10-07 11:59:46 +0200

    firstrune

--- firstrune.go
+++ firstrune.go
@@ -1,1 +1,9 @@
 package piscine
+
+
+func FirstRune() {
+	s := "trois"
+	result := []rune(s) //convertis les string en runes
+	FirstRune := string(result[0:1]) //convertis les runes de nouveau en string
+println(FirstRune)
+}
