96b5ddfe8457602b34fba3019beccb3d32905438
Yapi Théau <theau.yapi@ynov.comgit config --global user.name ytheau>
2022-10-10 14:54:17 +0200

    printstr

--- printstr.go
+++ printstr.go
@@ -3,9 +3,9 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintStr(s string) {
-r := []rune(s)
-l := len(r)
-for  i := 0; i < (l); i++ {
-	z01.PrintRune(r[i])
+	r := []rune(s)
+	l := len(r)
+	for  i := 0; i < (l); i++ {
+		z01.PrintRune(r[i])
 	}
 }
