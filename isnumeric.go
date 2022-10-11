9ff5195e52f5ab8542978c849683b7e575fded40
Yapi Théau <theau.yapi@ynov.comgit config --global user.name ytheau>
2022-10-11 15:59:15 +0200

    isnumeric

--- isnumeric.go
+++ isnumeric.go
@@ -4,11 +4,9 @@ func IsNumeric(s string) bool {
 	t := []rune(s)
 	a := true
 	for _, r := range t {
-		if r >= 48 && r <= 57 {
-			a = true
-		} else {
+		if !(r >= 48 && r <= 57) {
 			a = false
-		}
+		} 
 	}
 	return a
 }
