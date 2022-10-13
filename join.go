d9ea868c38de92e625447e33cf09d99600b90fd0
Yapi Théau <theau.yapi@ynov.comgit config --global user.name ytheau>
2022-10-13 11:45:30 +0200

    join

--- join.go
+++ join.go
@@ -1,9 +1,12 @@
 package piscine
 
 func Join(strs []string, sep string) string {
-	var final_string string
-	for _, s := range strs {
-		final_string += s + sep
-	}
-	return final_string 
+    var final_string string
+    for i, s := range strs {
+        final_string += s
+        if i != len(strs)-1 { // permet de ne pas mettre de ":" à la fin
+            final_string += sep
+        }
+    }
+    return final_string
 }
