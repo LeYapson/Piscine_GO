df83ca4a89cb2b1c7834cd81c0f6c623c8763d0d
Yapi Théau <theau.yapi@ynov.comgit config --global user.name ytheau>
2022-10-17 10:18:44 +0200

    trimatoi

--- alphacount.go
+++ alphacount.go
@@ -3,10 +3,29 @@ package piscine
 func AlphaCount(s string) int {
 	my_array := []rune(s)
 	k := 0
-	for _, r := range my_array {
+	for _, r := range my_array { // for r := 0; r <= len(my_array-1); r++
 		if r >= 65 && r <= 90 || r >= 97 && r <= 122 {
 			k++
 		}
 	}
 	return k
 }
+
+/*
+- résulat : le nombre de caractères de l'alphabet qu'il y a au total
+- 1 condition : ne doit pas compter les nombres
+- 2 condition : ne dois pas compter les caracteres spéciaux
+- 1: crée un tableau de rune
+2 crée une variable int que totalise le nombre de caractère aqu'il y a
+
+
+
+if r >= 65 && r <= 90(
+	k++
+) else if r >= 97 && r <= 122 (
+	k++
+)
+)
+return k
+
+*/
