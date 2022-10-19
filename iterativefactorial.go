e7e05d23cff3529b431ecc21dddf15fc6242c4c7
Théau Yapi <leyapson@pop-os.localdomain>
2022-10-19 11:48:54 +0200

    factorial

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -3,9 +3,9 @@ package piscine
 func IterativeFactorial(nb int) int {
 	if nb == 0 {
     	return 1
-    	}
+    }
 	result := 1
-	for i:= 1; i <= nb; i++ {
+	for i := 1; i <= nb; i++ {
 		result *= i
 	}
 	return result
