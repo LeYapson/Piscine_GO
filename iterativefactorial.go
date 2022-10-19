702801ea928b5be92b57692cb93376b30c827be0
Théau Yapi <leyapson@pop-os.localdomain>
2022-10-19 14:25:39 +0200

    factorial

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -4,11 +4,14 @@ func IterativeFactorial(nb int) int {
 	result := 0
 	if nb == 0 {
 		return result
-	} else if nb >= 1 {
-		result = 1
+	} else {
+		if nb >= 1 {
+			result = 1
 		for i := 1; i <= nb; i++ {
 			result *= i
 		}
+		}
+		
 	}
 	return result
 }
