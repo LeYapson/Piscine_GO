7053a282929d9a3c640038be4c71a52ddd5c6248
Théau Yapi <leyapson@pop-os.localdomain>
2022-10-20 11:00:49 +0200

    wallah la factorial est va marcher

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,14 +1,8 @@
 package piscine
 
 func IterativeFactorial(nb int) int {
-	if nb < 0 {
-		return 0
-	} else if nb == 0 {
+	if nb == 1 || nb == 0{
 		return 1
 	}
-	a := nb - 1
-	for i := a; i >= 1; i-- {
-		nb *= i
-	}
-	return nb
+	return nb * IterativeFactorial(nb-1)
 }
