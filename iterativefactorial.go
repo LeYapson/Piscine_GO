1d783989200ef7d52ec5f3c73c060d989b507428
Théau Yapi <leyapson@pop-os.localdomain>
2022-10-20 10:47:26 +0200

    wallah la factorial est va marcher

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -4,10 +4,9 @@ func IterativeFactorial(nb int) int {
 	var result int = 1
 	if nb < 0 {
 		result = 0
-	} else {
-		for i := 1; i <= nb; i++ {
-			result *= (i)
-		}
+	}
+	for i := 1; i <= nb; i++ {
+		result *= (i)
 	}
 	return result
 }
