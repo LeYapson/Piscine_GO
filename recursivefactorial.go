29c7374aa8d9d0061f1b07e7b42cc39c307427f4
Théau Yapi <leyapson@pop-os.localdomain>
2022-10-20 11:54:12 +0200

    wallah la factorial est va marcher

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -4,7 +4,7 @@ func RecursiveFactorial(nb int) int {
 	if nb == 1 || nb == 0 {
 		return 1
 	} else if nb > 1 && nb <= 25 {
-		nb *= RecursiveFactorial(nb-1)
+		nb *= RecursiveFactorial(nb - 1)
 	}
-	return nb 
+	return nb
 }
