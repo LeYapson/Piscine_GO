cf1e398e3539eabd549072b131c5181164e4c956
Théau Yapi <leyapson@pop-os.localdomain>
2022-10-20 17:12:34 +0200

    puissance c'est pas power mdr

--- iterativepower.go
+++ iterativepower.go
@@ -1,7 +1,9 @@
 package piscine
 
 func IterativePower(nb int, power int) int {
-	if power < 0 {
+	if nb ==0 && power == 0 {
+		return 1
+	} else if power < 0 {
 		return 0
 	} else if nb == 0 {
 		return 0
