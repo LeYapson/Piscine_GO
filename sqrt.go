63d5a232f3f1126ae86592b7697378f7d9c007f8
Théau Yapi <leyapson@pop-os.localdomain>
2022-10-21 11:24:29 +0200

    racine carre de singe

--- sqrt.go
+++ sqrt.go
@@ -7,7 +7,9 @@ func Sqrt(nb int) int {
 	var power float64 = 0.5
 	var nbr float64 = float64(nb)
 	nbr *= power
-	if int(nbr)%2 != 0 {
+	if nb == 1 {
+		return 1
+	} else if int(nbr)%2 != 0 {
 		return 0
 	} else if nb > 25 {
 		return 0
