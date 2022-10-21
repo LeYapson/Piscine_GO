3201d3c61c17c09ddc1566fc0fd696cf0548ba8f
Théau Yapi <leyapson@pop-os.localdomain>
2022-10-21 11:20:36 +0200

    racine carre de singe

---
+++ sqrt.go
@@ -0,0 +1,14 @@
+package piscine
+
+func Sqrt(nb int) int {
+	if nb <= 0 {
+		return 0
+	}
+	var power float64 = 0.5
+	var nbr float64 = float64(nb)
+	nbr *= power
+	if int(nbr) % 2 != 0 {
+		return 0
+	}
+	return int(nbr)
+}
