5d7675c7843b90bef35164bda75ec7956094eb29
Théau Yapi <leyapson@pop-os.localdomain>
2022-10-25 16:49:51 +0200

    j'en ai marre de la quest 4

--- isprime.go
+++ isprime.go
@@ -3,11 +3,11 @@ package piscine
 func IsPrime(nb int) bool {
 	if nb == 0 || nb == 1 {
 		return false
-	} else {  
-		for i := 2; i <= nb/2; i++ {  
-		 	if nb%i == 0 {
+	} else {
+		for i := 2; i <= nb/2; i++ {
+			if nb%i == 0 {
 				return false
-		 	}
+			}
 		}
 	}
 	return true
