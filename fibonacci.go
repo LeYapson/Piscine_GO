d9a760721407ef2929c9290cb93c019c8117eb36
Théau Yapi <leyapson@pop-os.localdomain>
2022-10-25 17:08:43 +0200

    c'est pas si dur fibo enfaite

--- fibonacci.go
+++ fibonacci.go
@@ -2,7 +2,7 @@ package piscine
 
 func Fibonacci(index int) int {
 	if index <= 1 {
-        return index
-    }
-    return Fibonacci(index-1) + Fibonacci(index-2)
+		return index
+	}
+	return Fibonacci(index-1) + Fibonacci(index-2)
 }
