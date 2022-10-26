241752079e134ed9c7ad7234256f4294fd34b20e
Théau Yapi <leyapson@pop-os.localdomain>
2022-10-26 18:20:02 +0200

    enfin zebi le point de mes deux

--- point/main.go
+++ point/main.go
@@ -6,8 +6,6 @@ import (
 	"github.com/01-edu/z01"
 )
 
-
-
 type point struct {
 	x int
 	y int
@@ -29,22 +27,22 @@ func main() {
 
 	for points.x != 0 {
 		eachValuex = points.x % 10
-		points.x /=10
+		points.x /= 10
 		arrayx = append(arrayx, rune(eachValuex))
 	}
 	for points.y != 0 {
 		eachValuey = points.y % 10
-		points.y /=10
+		points.y /= 10
 		arrayy = append(arrayy, rune(eachValuey))
 	}
 	text1 := "x = "
 	text2 := ", y = "
 	piscine.PrintStr(text1)
-	for i:= len(arrayx) -1; i >= 0; i-- {
+	for i := len(arrayx) - 1; i >= 0; i-- {
 		z01.PrintRune(arrayx[i] + 48)
 	}
 	piscine.PrintStr(text2)
-	for i:= len(arrayy) -1; i >= 0; i-- {
+	for i := len(arrayy) - 1; i >= 0; i-- {
 		z01.PrintRune(arrayy[i] + 48)
-	}	
+	}
 }
